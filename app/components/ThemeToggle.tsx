053abb102fb64e650396d39e485670222725f53d
ghoshOFahan <ahanghosh031004@gmail.com>
2025-12-17 23:19:32 +0530

    fix:Light theme button color fix

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -22,6 +22,7 @@ export default function ThemeToggle() {
         playClickSound("../../sounds/button.mp3");
       }}
       className="flex items-center gap-2 px-4 py-2 rounded-full border transition-all border-(--color-line) bg-(--color-bg) text-(--color-fg) hover:border-(--color-purple)"
+      // className="flex items-center gap-2 px-4 py-2 rounded-full border transition-all border-(--color-line) bg-(--color-bg) text-(--color-fg) hover:border-(--color-purple)"
     >
       {isDracula ? <Moon size={23} /> : <Sun size={23} />}
     </button>
